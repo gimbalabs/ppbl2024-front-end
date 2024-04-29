ee9f4c26445895a1cb5b72d16912367c81c36887
jamesdunseith <jdunseith@gimbalabs.io>
2024-04-29 14:26:09 -0400

    Add shadcn. Add links to home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,17 @@
 import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+import localFont from "next/font/local";
+
+const cera = localFont({ src: "./fonts/Cera-Pro-Regular.woff2" });
+const ceraBold = localFont({ src: "./fonts/Cera-Pro-Bold.woff2" });
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <main className={`font-sans ${inter.variable}`}>
+    <main className={`${cera.className}`}>
       <Component {...pageProps} />
     </main>
   );
